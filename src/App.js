6b349a296d8ab8de54111b81bbf5f4d0e773f47a
M-Phoenixx <mchuhan66@gmail.com>
2022-09-24 22:43:00 +0530

    local storage part left

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import './App.css';
 import Task from './Task'
+import { useState } from 'react';
 
 function App() {
+
+  const [input, setinput] = useState('')
+  
+  const inputvalue = (e) =>{
+    setinput(e.target.value)
+  }
+   
+   const add = (e) =>{
+    e.preventDefault();
+   }
+
+
   return (
     <>
      <div className="container">
+     <form action="submit">
       <div className="todo-box">
        <div className="todo-input">
-       <input type="text" />
-       <button >Add</button>
+       <input  onChange={inputvalue}  placeholder='Add work todo' type="text" />
+       <button onClick={add} >Add</button>
        </div>
        <div className="todo-tasks">
-         <Task/>
+         <Task input = {input} />
          <Task/>
          <Task/>
        </div>
-      </div>
+      </div></form>
      </div>
     </>
   );
--- src/Task.js
+++ src/Task.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import  './task.css'
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
-const Task = () => {
+const Task = ({input}) => {
   return (
-    <> <h1 className='task'> Hlo Moto! </h1>
+    <> <div className='task'> <p>hlo moto! {input} </p> <span> <EditIcon/> <DeleteIcon/></span>  </div>
+    
     </>
   )
 }
